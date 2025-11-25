56d72abb2034d969dbdac2c60528f60f9dfc5cfd
fateeva1717 <fateeva1717@gmail.com>
2025-11-25 20:26:12 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true
-  }
+  },
+  skipTrailingSlashRedirect: true
 }
 
 module.exports = nextConfig
